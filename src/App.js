61ddaf2371dbbbf14d078b8f024974dfbd28a73b
Elliott <ellbutt@gmail.com>
2024-12-04 21:45:51 -0330

    added new routes to allow for individual pages in the future

--- src/App.js
+++ src/App.js
@@ -13,12 +13,30 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+
+        {/* city related routes */}
         <Route path="/cities" element={<Cities />} />
+        <Route path="/cities/:id" element={<Cities />} />
+
+        {/* airport related routes */}
         <Route path="/airports" element={<Airports />} />
+        <Route path="/airports/:id" element={<Airports />} />
+
+        {/* airline related routes */}
         <Route path="/airlines" element={<Airlines />} />
+        <Route path="/airlines/:id" element={<Airlines />} />
+
+        {/* aircraft related routes */}
         <Route path="/aircraft" element={<Aircraft />} />
+        <Route path="/aircraft/:id" element={<Aircraft />} />
+
+        {/* flight related routes */}
         <Route path="/flights" element={<Flights />} />
+        <Route path="/flights/:id" element={<Flights />} />
+
+        {/* passenger related routes */}
         <Route path="/passengers" element={<Passengers />} />
+        <Route path="/passengers/:id" element={<Passengers />} />
       </Routes>
     </Router>
   );
